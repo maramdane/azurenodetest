25bf9dacc5f326102a51f384c483ea8479b5592a
maramdane <133639221+maramdane@users.noreply.github.com>
2023-09-25 16:29:29 +0200

    Update and rename app.js to app.py

--- app.js
+++
@@ -1,8 +0,0 @@
-// Ceci est un exemple de code Node.js LTS 18
-
-function afficherMessage() {
-    console.log("Bonjour, ceci est un exemple de code Node.js LTS 18 !");
-}
-
-// Appeler la fonction pour afficher le message
-afficherMessage();
